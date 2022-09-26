db1eab72f2cb5b030f17aa2b5adef0ec62f54825
Juan Antonio Sicardo Contreras <contacto@juansicardo.com>
2022-09-26 13:10:06 -0500

    Change mysql-db name

--- modules/data-stores/mysql/main.tf
+++ modules/data-stores/mysql/main.tf
@@ -1,10 +1,10 @@
-resource "aws_db_instance" "web_server_db" {
-  identifier_prefix   = "${var.db_name}-sql-db"
+resource "aws_db_instance" "mysql_db" {
+  identifier_prefix   = "${var.db_name}-db"
   engine              = "mysql"
   allocated_storage   = 10
   instance_class      = "db.t2.micro"
   skip_final_snapshot = true
-  db_name             = "${var.db_name}-sql-db"
+  db_name             = "${var.db_name}-db"
 
   # How should we set the username and password?
   username = var.db_username
